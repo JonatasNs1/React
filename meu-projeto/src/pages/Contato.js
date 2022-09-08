3d444c1cca3d9b028a4a3ea913f4eeee4afb6649
jonatasNs1 <jonatassantos152409@gmail.com>
2022-09-08 12:19:14 -0300

    react Router

---
+++ meu-projeto/src/pages/Contato.js
@@ -0,0 +1,14 @@
+function Contato(){
+
+    return (
+        <div>
+            <h1>
+                Contato
+            </h1>
+            <p> Conteudo da página</p>
+
+        </div>
+    )
+}
+
+export default Contato;
---
+++ meu-projeto/src/pages/Home.js
@@ -0,0 +1,14 @@
+function Home(){
+
+    return (
+        <div>
+            <h1>
+                Home
+            </h1>
+            <p> Conteudo da página</p>
+
+        </div>
+    )
+}
+
+export default Home;
