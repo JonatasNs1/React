deb77be81e0a945495a6e696a3895fbedd164fe0
jonatasNs1 <jonatassantos152409@gmail.com>
2022-09-08 10:41:05 -0300

    Renderização de listas

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -10,14 +10,21 @@ import List from './components/List';
 import Evento from './components/Evento';
 import Form from './components/Form';
 import Condicional from './components/Condicional';
+import OutraLista from './components/OutraLista';
+
+
+
+
 
 function App() {
 
+  const meusItens=['react', 'view', 'angular']
 
   return (
     <div className="App">
-      <h1> Renderização Condicional</h1>
-    <Condicional/>
+      <h1> Renderização de listas</h1>
+    <OutraLista itens={meusItens}/>
+    <OutraLista itens={[]} />
     </div>
   );
 }
